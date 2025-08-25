6675e56f5e2966e7d1523ca196765373467e04e2
ruokas <63348397+ruokas@users.noreply.github.com>
2025-08-25 17:51:06 +0300

    Add burn brush eraser and remove delete tool

--- public/js/BodyMapTools.js
+++ public/js/BodyMapTools.js
@@ -2,7 +2,8 @@ export const TOOLS = {
   WOUND: { char: 'Ž', symbol: '#sym-wound' },
   BRUISE: { char: 'S', symbol: '#sym-bruise' },
   BURN: { char: 'N', symbol: '#sym-burn' },
-  BURN_BRUSH: { char: 'B', symbol: '' }
+  BURN_BRUSH: { char: 'B', symbol: '' },
+  BURN_ERASE: { char: 'E', symbol: '' }
 };
 
 export default TOOLS;
