33ab97f54a507ab0e336f6fa9b4ad2489dc9a9ed
Rokas M <63348397+ruokas@users.noreply.github.com>
2025-08-26 16:09:58 +0300

    feat: enhance body map realism

--- docs/js/bodyMapZones.js
+++ docs/js/bodyMapZones.js
@@ -1,17 +1,17 @@
 export default [
   // Front zones
-  { id: 'front-head', side: 'front', path: 'M24 0a6 6 0 1 1 0 12a6 6 0 1 1 0-12z', area: 4.5, label: 'Galva (priekis)' },
-  { id: 'front-torso', side: 'front', path: 'M14 12c0-2 4-4 10-4s10 2 10 4v18c0 2-4 4-10 4s-10-2-10-4z', area: 18, label: 'Liemuo (priekis)' },
-  { id: 'front-left-arm', side: 'front', path: 'M0 12c-2 4-2 14 0 18h8V12z', area: 4.5, label: 'Kairė ranka (priekis)' },
-  { id: 'front-right-arm', side: 'front', path: 'M48 12c2 4 2 14 0 18h-8V12z', area: 4.5, label: 'Dešinė ranka (priekis)' },
-  { id: 'front-left-leg', side: 'front', path: 'M14 30v18h8V34c0-2-2-4-4-4z', area: 9, label: 'Kairė koja (priekis)' },
-  { id: 'front-right-leg', side: 'front', path: 'M26 30v18h8V34c0-2-2-4-4-4z', area: 9, label: 'Dešinė koja (priekis)' },
+  { id: 'front-head', side: 'front', path: 'M24 2a7 7 0 1 1 0 14a7 7 0 1 1 0-14z', area: 4.5, label: 'Galva (priekis)' },
+  { id: 'front-torso', side: 'front', path: 'M16 16c0-6 8-10 8-10s8 4 8 10v16c0 6-8 10-8 10s-8-4-8-10z', area: 18, label: 'Liemuo (priekis)' },
+  { id: 'front-left-arm', side: 'front', path: 'M2 16c-2 6-2 18 0 24h6V16z', area: 4.5, label: 'Kairė ranka (priekis)' },
+  { id: 'front-right-arm', side: 'front', path: 'M46 16c2 6 2 18 0 24h-6V16z', area: 4.5, label: 'Dešinė ranka (priekis)' },
+  { id: 'front-left-leg', side: 'front', path: 'M18 32c-4 4-4 12-4 16h8V36c0-4-2-4-4-4z', area: 9, label: 'Kairė koja (priekis)' },
+  { id: 'front-right-leg', side: 'front', path: 'M30 32c4 4 4 12 4 16h-8V36c0-4 2-4 4-4z', area: 9, label: 'Dešinė koja (priekis)' },
 
   // Back zones
-  { id: 'back-head', side: 'back', path: 'M24 0a6 6 0 1 1 0 12a6 6 0 1 1 0-12z', area: 4.5, label: 'Galva (nugara)' },
-  { id: 'back-torso', side: 'back', path: 'M14 12c0-2 4-4 10-4s10 2 10 4v18c0 2-4 4-10 4s-10-2-10-4z', area: 18, label: 'Liemuo (nugara)' },
-  { id: 'back-left-arm', side: 'back', path: 'M0 12c-2 4-2 14 0 18h8V12z', area: 4.5, label: 'Kairė ranka (nugara)' },
-  { id: 'back-right-arm', side: 'back', path: 'M48 12c2 4 2 14 0 18h-8V12z', area: 4.5, label: 'Dešinė ranka (nugara)' },
-  { id: 'back-left-leg', side: 'back', path: 'M14 30v18h8V34c0-2-2-4-4-4z', area: 9, label: 'Kairė koja (nugara)' },
-  { id: 'back-right-leg', side: 'back', path: 'M26 30v18h8V34c0-2-2-4-4-4z', area: 9, label: 'Dešinė koja (nugara)' }
+  { id: 'back-head', side: 'back', path: 'M24 2a7 7 0 1 1 0 14a7 7 0 1 1 0-14z', area: 4.5, label: 'Galva (nugara)' },
+  { id: 'back-torso', side: 'back', path: 'M16 16c0-6 8-10 8-10s8 4 8 10v16c0 6-8 10-8 10s-8-4-8-10z', area: 18, label: 'Liemuo (nugara)' },
+  { id: 'back-left-arm', side: 'back', path: 'M2 16c-2 6-2 18 0 24h6V16z', area: 4.5, label: 'Kairė ranka (nugara)' },
+  { id: 'back-right-arm', side: 'back', path: 'M46 16c2 6 2 18 0 24h-6V16z', area: 4.5, label: 'Dešinė ranka (nugara)' },
+  { id: 'back-left-leg', side: 'back', path: 'M18 32c-4 4-4 12-4 16h8V36c0-4-2-4-4-4z', area: 9, label: 'Kairė koja (nugara)' },
+  { id: 'back-right-leg', side: 'back', path: 'M30 32c4 4 4 12 4 16h-8V36c0-4 2-4 4-4z', area: 9, label: 'Dešinė koja (nugara)' }
 ];
